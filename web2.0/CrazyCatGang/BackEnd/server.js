fd60d80a16c20b92809588bcf1a113602936bb40
Ricardo-LK <92482482+Ricardo-LK@users.noreply.github.com>
2024-06-11 19:47:40 -0300

    Pagina adm, resolvendo bugs

--- web2.0/CrazyCatGang/BackEnd/server.js
+++ web2.0/CrazyCatGang/BackEnd/server.js
@@ -15,6 +15,8 @@ app.use(bodyParser.json());
 // Serve static files
 app.use(express.static(path.join(__dirname, '../', 'FrontEnd', 'formulario')));
 app.use(express.static(path.join(__dirname, '../', 'FrontEnd', 'SignIn')));
+app.use(express.static(path.join(__dirname, '../', 'FrontEnd', 'adm')));
+
 
 // Connect to MongoDB
 mongoose.connect("mongodb+srv://admin:8zmSR64N8UmtVEhT@clusterccg.yjs1evk.mongodb.net/cats?retryWrites=true&w=majority&appName=ClusterCCG", { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,8 +29,10 @@ const rescueRequestSchema = new mongoose.Schema({
     email: { type: String, required: true },
     phone: { type: String, required: true },
     address: { type: String, required: true },
-    characteristics: { type: String, required: true },
-    observations: { type: String, required: true },
+    characteristics: { type: String, required: false },
+    observations: { type: String, required: false },
+}, {
+    timestamps: true
 });
 
 const userSchema = new mongoose.Schema({
@@ -36,6 +40,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     phone: { type: String, required: true },
     password: { type: String, required: true },
+}, {
+    timeStamps: true
 });
 
 // Models
@@ -47,8 +53,22 @@ app.get('/rescueRequests', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'FrontEnd', 'formulario', 'formulario.html'));
 });
 
+app.get('/api/rescueRequests', async (req, res) => {
+    try {
+        const rescues = await RescueFormData.find({});
+        res.status(200).json(rescues)
+
+    } catch(err) {
+        res.status(500).json( {message: err.message} )
+    }
+})
+
 app.get('/signIn', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'FrontEnd', 'SignIn', 'cadastro.html'));
+});
+
+app.get('/adm', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'FrontEnd', 'adm', 'adm.html'));
 });
 
 app.get('/success', (req, res) => {
@@ -84,11 +104,23 @@ app.post('/api/signIn', async (req, res) => {
 // Handle rescue request submission
 app.post('/api/rescueRequests', async (req, res) => {
     const { name, email, phone, address, characteristics, observations } = req.body;
+    if (!name || !email || !phone || !address) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
 
     try {
+        const fiveMinutesAgo = new Date(Date.now() - 30000);
+
+        // Check for existing entry within the last 5 minutes
+        const recentRequest = await RescueFormData.findOne({ email, phone, createdAt: { $gte: fiveMinutesAgo } });
+
+        if (recentRequest) {
+            return res.status(409);
+        }
+
         const rescueFormData = new RescueFormData({ name, email, phone, address, characteristics, observations });
         await rescueFormData.save();
-        res.json({ message: 'Data saved successfully!' });
+        res.redirect('/success');
     } catch (err) {
         res.status(500).json({ message: 'Error saving data: ' + err.message });
     }
